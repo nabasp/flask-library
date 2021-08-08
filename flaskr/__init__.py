bdab274e355d28ae44898d65781c13072d84380c
nabasp <nabasp06@gmail.com>
2021-08-08 01:16:51 -0400

    deploy

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,8 +7,8 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.environ['DATABASE_URL'],
+        SECRET_KEY=os.environ.get('SECRET_KEY'),
+        DATABASE=os.environ.get('DATABASE_URL'),
     )
 
     if test_config is None:
