e9475476c81df3140713f11e9ac8896831f63eae
nabasp <nabasp06@gmail.com>
2021-08-07 22:46:34 -0400

    heroku deploy

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -42,6 +42,7 @@ def register():
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
+        return "posted"
         username = request.form['username']
         password = request.form['password']
         db = get_db()
